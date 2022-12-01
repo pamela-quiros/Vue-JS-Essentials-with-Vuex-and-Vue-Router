2a0c88e8fc7b105c75c5efb653b47bdd90c2f2ce
Pamela <pamela.quiros@tsi.com>
2022-12-01 17:04:00 -0600

    Section 3: Writing Effective Vue Apps

--- video-browser/src/App.vue
+++ video-browser/src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+        <SearchBar @termChange="onTermChange"></SearchBar>
+        <div class="row">
+            <VideoDetail :video="selectedVideo"/>
+            <VideoList @videoSelect="onVideoSelect" :videos="videos"></VideoList>
+        </div>
+  
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
+
+const API_KEY = 'AIzaSyBaqBOCs4Vdonp2rM0zQ1bs4_awUXXYrRA'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    components: { 
+        SearchBar,
+        VideoList ,
+        VideoDetail
+    },
+    data() {
+        return { videos: [], selectedVideo: null };
+    },
+    methods: {
+        onVideoSelect(video) {
+            this.selectedVideo = video
+        },
+        onTermChange(searchTerm) {
+            axios.get('https://www.googleapis.com/youtube/v3/search', {
+                params: {
+                    key: API_KEY,
+                    type: 'video',
+                    part: 'snippet',
+                    q: searchTerm
+                }
+            }).then(response => {
+                this.videos = response.data.items
+            })
+        }
+    }
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- video-browser/src/main.js
+++ video-browser/src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App'
 
-createApp(App).mount('#app')
+new Vue({
+    render: h  => h(App)
+}).$mount('#app');
